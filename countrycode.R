7b0785f351f1c2348400ae7d5886409e287618b4
malkuzweny <46692653+malkuzweny@users.noreply.github.com>
2020-05-10 12:03:29 -0700

    Add files via upload

--- countrycode.R
+++ countrycode.R
@@ -1,13 +1,18 @@
 library(rgdal)
 
-admn0_shp <- readOGR(dsn = "Downloads/ne_10m_admin_0_countries",
-                            "ne_10m_admin_0_countries")
+admn0_shp <- readOGR(dsn = "Downloads/ne_10m_admin_0_countries","ne_10m_admin_0_countries")
 
 admn0_df=admn0_shp@data
 
-getCountryCode <- function(countryname='countryname'){
-  iso3=as.character(droplevels(admn0_df$ADM0_A3[admn0_df$ADMIN==countryname]))
+getCountryCode <- function(countrynames){
+  iso3=c()
+  for (i in countrynames){
+    iso3val=as.character(droplevels(admn0_df$ADM0_A3[admn0_df$ADMIN==i]))
+    iso3=c(iso3,iso3val)
+  }
   return(iso3)
 }
 
-getCountryCode('India')
+#test
+print(getCountryCode(list('India', 'Argentina')))
+

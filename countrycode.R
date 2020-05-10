fb01fac79b583a0f5f85040d39472ee28274e610
malkuzweny <46692653+malkuzweny@users.noreply.github.com>
2020-05-10 12:47:41 -0700

    Add files via upload

--- countrycode.R
+++ countrycode.R
@@ -16,3 +16,31 @@ getCountryCode <- function(countrynames){
 #test
 print(getCountryCode(list('India', 'Argentina')))
 
+library(tidyverse)
+library(jsonlite)
+
+api.url <- "https://api.outbreak.info/v1/"
+
+getCountryData <- function(location_names){
+  locations=getCountryCode(location_names)
+  scroll.id <- NULL
+  location.ids <- paste0("%22", paste(locations, collapse="%22%20OR%20%22"), "%22")
+  results <- list()
+  success <- NULL
+  while(is.null(success)){
+    dataurl <- paste0(api.url, "query?q=location_id:(",location.ids,")&sort=date&size=1000&fetch_all=true")
+    dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
+    resp <- fromJSON(dataurl, flatten=TRUE)
+    scroll.id <- resp$`_scroll_id`
+    results[[length(results) + 1]] <- resp$hits
+    success <- resp$success
+  }
+  hits <- rbind_pages(results)
+  return(hits);
+}
+
+#test
+df <- getCountryData(c('India','Argentina','Iraq'))
+df
+
+

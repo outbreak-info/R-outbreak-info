cfe32fd53be2f9143dd4edbe978396559eb87ed0
malkuzweny <46692653+malkuzweny@users.noreply.github.com>
2020-06-01 15:53:04 -0700

    Add files via upload

--- locationcode.R
+++ locationcode.R
@@ -1,6 +1,6 @@
 library(rgdal)
-library(tidyverse)
 library(jsonlite)
+library(ggplot2)
 
 country_shp <- readOGR(dsn="Downloads/ne_10m_admin_0_countries","ne_10m_admin_0_countries")
 country_df=country_shp@data
@@ -8,19 +8,21 @@ county_shp <- readOGR(dsn="Downloads/tl_2019_us_county", "tl_2019_us_county")
 county_df=county_shp@data
 state_shp <- readOGR(dsn="Downloads/ne_10m_admin_1_states_provinces", "ne_10m_admin_1_states_provinces")
 state_df=state_shp@data
+metro_shp <- readOGR(dsn="Downloads/cb_2018_us_cbsa_500k", "cb_2018_us_cbsa_500k")
+metro_df=metro_shp@data
 
 getLocationCode <- function(locationnames){
   iso3codes=c()
   for (i in locationnames){
     if (i %in% country_df$ADMIN){
       countryiso3val=as.character(droplevels(country_df$ADM0_A3[country_df$ADMIN==i]))
-      iso3codes=c(iso3codes,countryiso3val)
+      iso3codes=c(iso3codes, countryiso3val)
     }
     if (i %in% state_df$name){
       iso3=levels(as.factor(as.character(state_df$iso_3166_2[state_df$name==i])))
       countryname=as.character(droplevels(state_df$gu_a3[state_df$iso_3166_2==iso3]))
       stateiso3val=paste0(countryname, "_", iso3)
-      iso3codes=c(iso3codes,stateiso3val)
+      iso3codes=c(iso3codes, stateiso3val)
     }
     if (i %in% county_df$NAMELSAD){
       sfips=as.character(droplevels(county_df$STATEFP[county_df$NAMELSAD==i]))
@@ -31,6 +33,11 @@ getLocationCode <- function(locationnames){
       countryname=as.character(droplevels(state_df$gu_a3[state_df$iso_3166_2==iso3]))
       countyiso3val=paste0(countryname, "_", iso3, "_", fipsval)
       iso3codes=c(iso3codes, countyiso3val)
+    }
+    if (i %in% metro_df$NAME){
+      mfips=as.character(droplevels(metro_df$CBSAFP[metro_df$NAME==i]))
+      metroiso3val=paste0("METRO_", mfips)
+      iso3codes=c(iso3codes, metroiso3val)
     }
   }
   if (length(iso3codes)<length(locationnames)){
@@ -43,10 +50,10 @@ return(iso3codes)
 }
 #must enter [county name] County
 
+#testing
 getLocationCode(c("India", "Virginia", "San Diego County"))
 getLocationCode(c("Berkeley County", "Arizona"))
 getLocationCode(c("Rusia", "Hawaii County"))
-#testing
 
 api.url <- "https://api.outbreak.info/v1/"
 
@@ -69,4 +76,6 @@ getLocationData <- function(location_names){
 }
 
 #test
-getLocationData(c("India", "Virginia", "San Diego County"))
+df <- getLocationData(c("India", "Virginia", "San Diego County"))
+
+

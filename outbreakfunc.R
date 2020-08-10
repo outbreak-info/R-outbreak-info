c3400562f94b8a4c3da1f5057c56c7e075104c72
Manar Alkuzweny <>
2020-08-10 10:00:22 -0700

    updated getISO3, searchLocations

--- outbreakfunc.R
+++ outbreakfunc.R
@@ -5,44 +5,10 @@ api.url <- "https://api.outbreak.info/v1/"
 
 getISO3 <- function(locations_to_search){
   locs_of_interest=c()
-  scroll.id <- NULL
-  location.ids <- paste0("(name:%22", paste(locations_to_search, collapse="%22%20OR%20name:%22"), "%22)")
-  results <- list()
-  success <- NULL
-  while(is.null(success)){
-    dataurl <- paste0(api.url, "query?q=",location.ids,"%20AND%20mostRecent:true&fields=name,location_id,state_name&fetch_all=true")
-    dataurl <- gsub(" ", "+", dataurl)
-    dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-    resp <- fromJSON(dataurl, flatten=TRUE)
-    scroll.id <- resp$'_scroll_id'
-    results[[length(results) + 1]] <- resp$hits
-    success <- resp$success
-  }
-  t <- try(rbind_pages(results), silent=T)
-  if("try-error" %in% class(t)){
-    error=T
-  }else{
-    hits <- rbind_pages(results)
-    df=(hits)
-    locs_of_interest=df$location_id
-  }
-  if (length(locs_of_interest)==length(locations_to_search)){
-    return(locs_of_interest)
-  }
-  if (length(locs_of_interest)!=length(locations_to_search)||error==T){
-    locations=c()
-    locs_of_interest=c()
-    for (i in locations_to_search){
-      if (grepl(" ", i, fixed=TRUE)==T){
-        locs=paste0("*",i,"*")
-        locs=gsub(" ", "*", locs, fixed=TRUE)
-      }else{
-        locs=paste0("*",i,"*")
-      }
-      locations=c(locations, locs)
-    }
+  locs_not_found=c()
+  for (i in locations_to_search){
     scroll.id <- NULL
-    location.ids <- paste0("(name:", paste(locations, collapse="%20OR%20name:"), ")")
+    location.ids <- paste0("(name:%22", paste(i, collapse="%22%20OR%20name:%22"), "%22)")
     results <- list()
     success <- NULL
     while(is.null(success)){
@@ -54,21 +20,72 @@ getISO3 <- function(locations_to_search){
       results[[length(results) + 1]] <- resp$hits
       success <- resp$success
     }
-    hits <- rbind_pages(results)
-    df=(hits)
-    df$name=apply(cbind(df$name, df$state_name), 1, function(x) paste(x[!is.na(x)], collapse = ", "))
-    for (i in df$name){
-      print(i)
-      loc_sel <- readline("Is this a location of interest? (Y/N): ")
-      if ((loc_sel == "Y")|(loc_sel == "y")){
-        locs_of_interest = c(locs_of_interest, df$location_id[df$name==i])
+    t <- try(rbind_pages(results), silent=T)
+    if("try-error" %in% class(t)){
+      error=T
+      locs_not_found = c(locs_not_found, i)
+    }else{
+      hits <- rbind_pages(results)
+      df=(hits)
+      if (nrow(df)==1){
+        locs_of_interest=c(locs_of_interest, df$location_id)
+      }else{
+        locs_not_found=c(locs_not_found, i)
       }
-      if ((loc_sel != "Y")&(loc_sel != "y")&(loc_sel != "N")&(loc_sel != "n")){
-        print("Expected input is Y or N")
+    }
+  }
+  if (length(locs_of_interest)==length(locations_to_search)){
+    return(locs_of_interest)
+  }
+  if (length(locs_of_interest)!=length(locations_to_search)){
+    locations=c()
+    for (i in locs_not_found){
+      if (grepl(" ", i, fixed=TRUE)==T){
+        locs=paste0("*",i,"*")
+        locs=gsub(" ", "*", locs, fixed=TRUE)
+      }else{
+        locs=paste0("*",i,"*")
+      }
+      locations=c(locations, locs)
+    }
+    for (i in 1:length(locations)){
+      scroll.id <- NULL
+      location.ids <- paste0("(name:", paste(locations[i], collapse="%20OR%20name:"), ")")
+      results <- list()
+      success <- NULL
+      while(is.null(success)){
+        dataurl <- paste0(api.url, "query?q=",location.ids,"%20AND%20mostRecent:true&fields=name,location_id,state_name&fetch_all=true")
+        dataurl <- gsub(" ", "+", dataurl)
+        dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
+        resp <- fromJSON(dataurl, flatten=TRUE)
+        scroll.id <- resp$'_scroll_id'
+        results[[length(results) + 1]] <- resp$hits
+        success <- resp$success
+      }
+      t2 <- try(rbind_pages(results), silent=T)
+      if("try-error" %in% class(t2)){
+        print(paste(locs_not_found[i], "not found. Please check spelling."))
+        next
+      }else{
+        hits <- rbind_pages(results)
+        df=(hits)
+        df$name=apply(cbind(df$name, df$state_name), 1, function(x) paste(x[!is.na(x)], collapse = ", "))
+      }
+      for (i in df$name){
         print(i)
         loc_sel <- readline("Is this a location of interest? (Y/N): ")
         if ((loc_sel == "Y")|(loc_sel == "y")){
           locs_of_interest = c(locs_of_interest, df$location_id[df$name==i])
+          break
+        }
+        if ((loc_sel != "Y")&(loc_sel != "y")&(loc_sel != "N")&(loc_sel != "n")){
+          print("Expected input is Y or N")
+          print(i)
+          loc_sel <- readline("Is this a location of interest? (Y/N): ")
+          if ((loc_sel == "Y")|(loc_sel == "y")){
+            locs_of_interest = c(locs_of_interest, df$location_id[df$name==i])
+            break
+          }
         }
       }
     }
@@ -78,34 +95,41 @@ getISO3 <- function(locations_to_search){
 
 searchLocations <- function(locations_to_search, admin_level){
   locs_of_interest=c()
-  scroll.id <- NULL
-  location.ids <- paste0("(name:%22", paste(locations_to_search, collapse="%22%20OR%20name:%22"), "%22)")
-  results <- list()
-  success <- NULL
-  while(is.null(success)){
-    dataurl <- paste0(api.url, "query?q=admin_level:", admin_level, "%20AND%20(",location.ids,")%20AND%20mostRecent:true&fields=name,location_id,state_name&fetch_all=true")
-    dataurl <- gsub(" ", "+", dataurl)
-    dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-    resp <- fromJSON(dataurl, flatten=TRUE)
-    scroll.id <- resp$'_scroll_id'
-    results[[length(results) + 1]] <- resp$hits
-    success <- resp$success
-  }
-  t <- try(rbind_pages(results), silent=T)
-  if("try-error" %in% class(t)){
-    error=T
-  }else{
-    hits <- rbind_pages(results)
-    df=(hits)
-    locs_of_interest=df$location_id
+  locs_not_found=c()
+  for (i in locations_to_search){
+    scroll.id <- NULL
+    location.ids <- paste0("(name:%22", paste(i, collapse="%22%20OR%20name:%22"), "%22)")
+    results <- list()
+    success <- NULL
+    while(is.null(success)){
+      dataurl <- paste0(api.url, "query?q=", location.ids, "%20AND%20", "admin_level:%22", admin_level, "%22", "%20AND%20mostRecent:true&fields=name,location_id,state_name&fetch_all=true")
+      dataurl <- gsub(" ", "+", dataurl)
+      dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
+      resp <- fromJSON(dataurl, flatten=TRUE)
+      scroll.id <- resp$'_scroll_id'
+      results[[length(results) + 1]] <- resp$hits
+      success <- resp$success
+    }
+    t <- try(rbind_pages(results), silent=T)
+    if("try-error" %in% class(t)){
+      error=T
+      locs_not_found = c(locs_not_found, i)
+    }else{
+      hits <- rbind_pages(results)
+      df=(hits)
+      if (nrow(df)==1){
+        locs_of_interest=c(locs_of_interest, i)
+      }else{
+        locs_not_found=c(locs_not_found, i)
+      }
+    }
   }
   if (length(locs_of_interest)==length(locations_to_search)){
-    return(df$name)
+    return(locs_of_interest)
   }
-  if ((length(locs_of_interest)!=length(locations_to_search))|error==T){
+  if (length(locs_of_interest)!=length(locations_to_search)){
     locations=c()
-    locs_of_interest=c()
-    for (i in locations_to_search){
+    for (i in locs_not_found){
       if (grepl(" ", i, fixed=TRUE)==T){
         locs=paste0("*",i,"*")
         locs=gsub(" ", "*", locs, fixed=TRUE)
@@ -114,60 +138,49 @@ searchLocations <- function(locations_to_search, admin_level){
       }
       locations=c(locations, locs)
     }
-    scroll.id <- NULL
-    location.ids <- paste0("(name:", paste(locations, collapse="%20OR%20name:"), ")")
-    results <- list()
-    success <- NULL
-    while(is.null(success)){
-      dataurl <- paste0(api.url, "query?q=admin_level:", admin_level, "%20AND%20(",location.ids,")%20AND%20mostRecent:true&fields=name,location_id,state_name&fetch_all=true")
-      dataurl <- gsub(" ", "+", dataurl)
-      dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-      resp <- fromJSON(dataurl, flatten=TRUE)
-      scroll.id <- resp$'_scroll_id'
-      results[[length(results) + 1]] <- resp$hits
-      success <- resp$success
-    }
-    hits <- rbind_pages(results)
-    df=hits
-    df$name=apply(cbind(df$name, df$state_name), 1, function(x) paste(x[!is.na(x)], collapse = ", "))
-    for (i in df$name){
-      print(i)
-      loc_sel <- readline("Is this a location of interest? (Y/N): ")
-      if ((loc_sel == "Y")|(loc_sel == "y")){
-        locs_of_interest = c(locs_of_interest, i)
+    for (i in 1:length(locations)){
+      scroll.id <- NULL
+      location.ids <- paste0("(name:", paste(locations[i], collapse="%20OR%20name:"), ")")
+      results <- list()
+      success <- NULL
+      while(is.null(success)){
+        dataurl <- paste0(api.url, "query?q=", location.ids, "%20AND%20", "admin_level:%22", admin_level, "%22", "%20AND%20mostRecent:true&fields=name,location_id,state_name&fetch_all=true")
+        dataurl <- gsub(" ", "+", dataurl)
+        dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
+        resp <- fromJSON(dataurl, flatten=TRUE)
+        scroll.id <- resp$'_scroll_id'
+        results[[length(results) + 1]] <- resp$hits
+        success <- resp$success
       }
-      if ((loc_sel != "Y")&(loc_sel != "y")&(loc_sel != "N")&(loc_sel != "n")){
-        print("Expected input is Y or N")
+      t2 <- try(rbind_pages(results), silent=T)
+      if("try-error" %in% class(t2)){
+        print(paste(locs_not_found[i], "not found. Please check spelling."))
+        next
+      }else{
+        hits <- rbind_pages(results)
+        df=(hits)
+        df$name=apply(cbind(df$name, df$state_name), 1, function(x) paste(x[!is.na(x)], collapse = ", "))
+      }
+      for (i in df$name){
         print(i)
         loc_sel <- readline("Is this a location of interest? (Y/N): ")
         if ((loc_sel == "Y")|(loc_sel == "y")){
           locs_of_interest = c(locs_of_interest, i)
+          break
+        }
+        if ((loc_sel != "Y")&(loc_sel != "y")&(loc_sel != "N")&(loc_sel != "n")){
+          print("Expected input is Y or N")
+          print(i)
+          loc_sel <- readline("Is this a location of interest? (Y/N): ")
+          if ((loc_sel == "Y")|(loc_sel == "y")){
+            locs_of_interest = c(locs_of_interest, i)
+            break
+          }
         }
       }
     }
   }
   return(locs_of_interest)
-}
-
-getLocationCodes <- function(location_names, admin_level){
-  locations <- searchLocations(location_names, admin_level)
-  scroll.id <- NULL
-  location.ids <- paste0("%22", paste(locations, collapse="%22%20OR%20%22"), "%22")
-  results <- list()
-  success <- NULL
-  while(is.null(success)){
-    dataurl <- paste0(api.url, "query?q=name:(",location.ids,")&fetch_all=true&sort=-date&fields=location_id")
-    dataurl <- gsub(" ", "+", dataurl)
-    dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-    resp <- fromJSON(dataurl, flatten=TRUE)
-    scroll.id <- resp$'_scroll_id'
-    results[[length(results) + 1]] <- resp$hits
-    success <- resp$success
-  }
-  hits <- rbind_pages(results)
-  df=hits
-  iso3=unique(df$location_id)
-  return(iso3)
 }
 
 getLocationData <- function(location_names){
@@ -223,6 +236,7 @@ getAdmn2ByCountry <- function(){
   return(hits)
 }
 
+
 getAdmn1ByCountry <- function(countries){
   locations <- searchLocations(countries, admin_level=0)
   scroll.id <- NULL
@@ -230,7 +244,7 @@ getAdmn1ByCountry <- function(countries){
   location.ids <- paste0("%22", paste(locations, collapse="%22%20OR%20%22"), "%22")
   success <- NULL
   while(is.null(success)){
-    dataurl <- paste0(api.url, "query?q=country_name:(",location.ids,")&fetch_all=true&sort=-date&admin_level=1")
+    dataurl <- paste0(api.url, "query?q=country_name:(",location.ids,")%20AND%20admin_level:1&fetch_all=true")
     dataurl <- gsub(" ", "+", dataurl)
     dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
     resp <- fromJSON(dataurl, flatten=TRUE)
@@ -258,10 +272,31 @@ getMetroByCountry <- function(){
   return(hits)
 }
 
+getCountryByRegion <- function(wb_regions){
+  locations <- searchLocations(wb_regions, admin_level=-1)
+  scroll.id <- NULL
+  results <- list()
+  location.ids <- paste0("%22", paste(locations, collapse="%22%20OR%20%22"), "%22")
+  success <- NULL
+  while(is.null(success)){
+    dataurl <- paste0(api.url, "query?q=wb_region:(",location.ids,")%20AND%20admin_level:0&fetch_all=true")
+    dataurl <- gsub(" ", "+", dataurl)
+    dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
+    resp <- fromJSON(dataurl, flatten=TRUE)
+    scroll.id <- resp$'_scroll_id'
+    results[[length(results) + 1]] <- resp$hits
+    success <- resp$success
+  }
+  hits <- rbind_pages(results)
+  return(hits)
+}
+
 plotCovid <- function(locations, key){
   df <- getLocationData(locations)
   df$date=as.Date(df$date, "%Y-%m-%d")
   p=ggplot(df, aes(date, get(key), color=name, group = name)) + geom_line() + scale_x_date(date_breaks = "1 week") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + labs(y=key)
+  return(p)
 }
+
 
 
--- outbreakinfo/.Rproj.user/EA66C9AB/sources/s-FC6548A3/11074B38-contents
+++ outbreakinfo/.Rproj.user/EA66C9AB/sources/s-FC6548A3/11074B38-contents
@@ -1,26 +1,25 @@
-#' @title Retrieve COVID-19 data for states/provinces in a country or countries
+#' @title Retrieve COVID-19 data for countries in a World Bank region(s)
 #'
-#' @description Retrieve up-to-date COVID-19 data from outbreak.info for all states/provinces in one or more countries.
+#' @description Retrieve up-to-date COVID-19 data from outbreak.info for all countries in one or more World Bank region(s).
 #'
-#'
-#' @param countries: vector or list of country name(s)
+#' @param wb_regions: vector or list of World Bank region(s)
 #'
 #' @return dataframe
 #'
 #' @examples
-#' getAdmn1ByCountry("India")
+#' getCountryByRegion("North America")
 #'
 #' @export
 #' @import jsonlite
 
-getAdmn1ByCountry <- function(countries){
-  locations <- searchLocations(countries, admin_level=0)
+getCountryByRegion <- function(wb_regions){
+  locations <- searchLocations(wb_regions, admin_level=-1)
   scroll.id <- NULL
   results <- list()
   location.ids <- paste0("%22", paste(locations, collapse="%22%20OR%20%22"), "%22")
   success <- NULL
   while(is.null(success)){
-    dataurl <- paste0(api.url, "query?q=country_name:(",location.ids,")%20AND%20admin_level:1&fetch_all=true")
+    dataurl <- paste0(api.url, "query?q=wb_region:(",location.ids,")%20AND%20admin_level:0&fetch_all=true")
     dataurl <- gsub(" ", "+", dataurl)
     dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
     resp <- fromJSON(dataurl, flatten=TRUE)

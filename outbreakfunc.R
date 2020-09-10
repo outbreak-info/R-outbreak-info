7ad46555d3972393aca4b437d601f9d7fae90862
Manar Alkuzweny <>
2020-09-10 15:49:57 -0700

    added getEpiData function

--- outbreakfunc.R
+++ outbreakfunc.R
@@ -202,6 +202,7 @@ getLocationData <- function(location_names){
   return(hits);
 }
 
+
 getAdmn2ByState <- function(states){
   locations <- searchLocations(states, admin_level=1)
   scroll.id <- NULL
@@ -257,6 +258,7 @@ getAdmn1ByCountry <- function(countries){
   return(hits)
 }
 
+
 getMetroByCountry <- function(){
   scroll.id <- NULL
   results <- list()
@@ -296,7 +298,7 @@ getCountryByRegion <- function(wb_regions){
 plotCovid <- function(locations, key){
   df <- getLocationData(locations)
   df$date=as.Date(df$date, "%Y-%m-%d")
-  p=ggplot(df, aes(date, get(key), color=name, group = name)) + geom_line() + scale_x_date(date_breaks = "1 week") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + labs(y=key)
+  p=ggplot(df, aes(date, get(key), color=name, group=name)) + geom_line() + scale_x_date(date_breaks = "1 week") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + labs(y=key)
   return(p)
 }
 
@@ -308,10 +310,57 @@ df=data.frame("API Field"=c("admin_level", "cbsa", "confirmed", "confirmed_doubl
 return(df)
 }
 
-
-
-
-
-
+getEpiData <- function(name=NULL, location_id=NULL, wb_region=NULL, country_name=NULL, state_name=NULL, admin_level=NULL, date=NULL, mostRecent=NULL, fields=NULL, sort=NULL, size=NULL){
+  q <- c()
+  if(!is.null(name)){
+    q <- c(q, paste0("(name:%22", paste(name, collapse="%22%20OR%20name:%22"), "%22)%20AND%20"))
+  }
+  if(!is.null(location_id)){
+    q <- c(q, paste0("(location_id:%22", paste(location_id, collapse="%22%20OR%20location_id:%22"), "%22)%20AND%20"))
+  }
+  if(!is.null(wb_region)){
+    q <- c(q, paste0("(wb_region:%22", paste(wb_region, collapse="%22%20OR%20wb_region:%22"), "%22)%20AND%20"))
+  }
+  if(!is.null(country_name)){
+    q <- c(q, paste0("(country_name:%22", paste(country_name, collapse="%22%20OR%20country_name:%22"), "%22)%20AND%20"))
+  }
+  if(!is.null(state_name)){
+    q <- c(q, paste0("(state_name:%22", paste(state_name, collapse="%22%20OR%20state_name:%22"), "%22)%20AND%20"))
+  }
+  if(!is.null(admin_level)){
+    q <- c(q, paste0("(admin_level:", paste(admin_level, collapse="%20OR%20admin_level:"), ")%20AND%20"))
+  }
+  if(!is.null(date)){
+    q <- c(q, paste0("(date:", paste(date, collapse="%20OR%20date:"), ")%20AND%20"))
+  }
+  if(!is.null(mostRecent)){
+    q <- c(q, paste0("mostRecent:", paste(mostRecent)))
+  }
+  if(!is.null(fields)){
+    q <- c(q, paste0("&fields=", paste(fields, collapse=",")))
+  }
+  if(!is.null(sort)){
+    q <- c(q, paste0("&sort=", paste(sort)))
+  }
+  if(!is.null(size)){
+    q <- c(q, paste0("&size=", paste(sort)))
+  }
+  q <- paste(q, sep="", collapse = "")
+  q <- paste0(q, "&fetch_all=true")
+  
+  scroll.id <- NULL
+  results <- list()
+  success <- NULL
+  while(is.null(success)){
+    dataurl <- paste0(api.url, "query?q=", q)
+    dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
+    resp <- fromJSON(dataurl, flatten=TRUE)
+    scroll.id <- resp$'_scroll_id'
+    results[[length(results) + 1]] <- resp$hits
+    success <- resp$success
+  }
+  hits <- rbind_pages(results)
+  return(hits)
+}
 
 

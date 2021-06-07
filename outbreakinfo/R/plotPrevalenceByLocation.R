672c9232437015fae102f0fa118d5db4661132da
Manar Alkuzweny <>
2021-06-07 13:50:41 -0700

    fixes #32

--- outbreakinfo/R/plotPrevalenceByLocation.R
+++ outbreakinfo/R/plotPrevalenceByLocation.R
@@ -6,6 +6,7 @@
 #'@param location: a location name
 #'@param mutations: (optional) a `vector` of mutation(s)
 #'@param cumulative: (optional) `Boolean` (T/F), T returns cumulative prevalence since first day of detection
+#'@param include_title: `Boolean` (T/F), T returns plot with title, F returns plot without title (default=F)
 #'
 #'@return ggplot2 object
 #'
@@ -18,9 +19,12 @@
 #' @import ggplot2
 
 
-plotPrevalenceByLocation <- function(pangolin_lineage, location, mutations=NULL, cumulative=NULL){
+plotPrevalenceByLocation <- function(pangolin_lineage, location, mutations=NULL, cumulative=NULL, include_title=F){
   df <- getGenomicData(query_url="prevalence-by-location", pangolin_lineage = pangolin_lineage, location = location, mutations = mutations, cumulative = cumulative)
-  p <- ggplot(data=df, aes(x=date, y=proportion)) + geom_line()
+  p <- ggplot(data=df, aes(x=date, y=proportion)) + geom_line() + scale_y_continuous(labels = scales::percent, name="percentage")
   p <- p + geom_ribbon(aes(ymin=proportion_ci_lower, ymax=proportion_ci_upper), alpha=0.2)
+  if (include_title == T){
+    p <- p + ggtitle(paste0("Prevalence of ", pangolin_lineage, " in ", location))
+  }
   return(p)
 }

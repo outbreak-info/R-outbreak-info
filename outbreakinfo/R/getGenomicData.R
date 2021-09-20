2052ff53d74134f4e9f9ba14123fd6c8859ee78d
gkarthik <xraydiffraction@gmail.com>
2021-09-19 20:30:50 -0700

    Add function to parse response with list

--- outbreakinfo/R/getGenomicData.R
+++ outbreakinfo/R/getGenomicData.R
@@ -11,8 +11,31 @@
 #' @export
 #' @import jsonlite
 
+## If object is a list, return a long dataframe with query_key as new column.
+## If object is a dataframe, return the object directly
+## If object is neither, throw warning and return NULL
+convert_list_to_dataframe <- function(list_obj){
+    if(!(class(list_obj) %in% c("list", "data.frame"))){
+        warning("Supplied object is not a list or dataframe")
+        return(NULL)
+    }
+    if(class(list_obj) == "data.frame"){
+        return(list_obj)
+    }
+    ## If list add a "query_key" column
+    query_keys <- names(list_obj)
+    res <- lapply(query_keys,
+           function(query_key) {
+               d <- list_obj[[query_key]]
+               d$query_key <- query_key
+               return(d)
+           })
+    res_df <- do.call(rbind, out)
+    return(res_df);
+}
+
 getGenomicData <- function(query_url, location=NULL, cumulative=NULL, pangolin_lineage=NULL, mutations=NULL, ndays=NULL, frequency=NULL, subadmin=NULL, other_threshold=NULL, nday_threshold=NULL, other_exclude=NULL){
-  genomic_url <- "http://localhost:8000/genomics/"
+  genomic_url <- "https://dev.outbreak.info/genomics/"
 
   q <- c()
 
@@ -71,12 +94,20 @@ getGenomicData <- function(query_url, location=NULL, cumulative=NULL, pangolin_l
   while(is.null(success)){
       success <- FALSE
     cat("Retrieving data...", "\n")
-    dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-    tryCatch({
-        resp <- GET(
-            dataurl,
-            add_headers(Authorization = paste("Bearer", Sys.getenv("OUTBREAK_INFO_TOKEN"), sep=" "))
-        )
+      dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
+      dataurl <- URLencode(dataurl)
+      resp <- NULL
+      tryCatch({
+          if(Sys.getenv("OUTBREAK_INFO_TOKEN") != ""){
+              resp <- GET(
+                  dataurl,
+                  add_headers(Authorization = paste("Bearer", Sys.getenv("OUTBREAK_INFO_TOKEN"), sep=" "))
+              )
+          } else {
+              resp <- GET(
+                  dataurl
+              )
+          }
         auth_token = resp$headers$`x-auth-token`
         if(!is.null(auth_token))
             Sys.setenv(OUTBREAK_INFO_TOKEN = auth_token)
@@ -92,7 +123,8 @@ getGenomicData <- function(query_url, location=NULL, cumulative=NULL, pangolin_l
             warning("Malformed token. Please reauthenticate by calling the authenticate() function.")
         } else if(resp$status_code == 200){
             resp <- fromJSON(content(resp, "text"), flatten=TRUE)
-            results[[length(results) + 1]] <- resp$results
+            resp_df <- convert_list_to_dataframe(resp$results)
+            results[[length(results) + 1]] <- resp_df
             scroll.id <- resp$'_scroll_id'
             success <- resp$success
         }
@@ -113,5 +145,3 @@ getGenomicData <- function(query_url, location=NULL, cumulative=NULL, pangolin_l
   }
   return(hits)
 }
-
-df <- getGenomicData(query_url="sequence-count", location = "United States", cumulative = FALSE, subadmin = FALSE)

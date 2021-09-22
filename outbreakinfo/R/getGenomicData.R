1b1665e15ed5482beacd10e50207f1300c09f204
gkarthik <xraydiffraction@gmail.com>
2021-09-21 21:40:46 -0700

    Restructured to get response from separate function

--- outbreakinfo/R/getGenomicData.R
+++ outbreakinfo/R/getGenomicData.R
@@ -11,141 +11,71 @@
 #' @export
 #' @import jsonlite
 
+
 getGenomicData <- function(query_url, location=NULL, cumulative=NULL, pangolin_lineage=NULL, mutations=NULL, ndays=NULL, frequency=NULL, subadmin=NULL, other_threshold=NULL, nday_threshold=NULL, other_exclude=NULL){
 
-    ## If object is a list, return a long dataframe with query_key as new column.
-    ## If object is a dataframe, return the object directly
-    ## If object is neither, throw warning and return NULL
-    convert_list_to_dataframe <- function(list_obj){
-        if(!(class(list_obj) %in% c("list", "data.frame"))){
-            warning("Supplied object is not a list or dataframe")
-            return(NULL)
+    genomic_url <- "https://dev.outbreak.info/genomics/"
+
+    q <- c()
+
+    q <- c(q, paste0(query_url), "?")
+
+    if(!is.null(location)){
+        location <- getISO3_genomic(location)
+        q <- c(q, paste0("location_id=", location, "&"))
+    }
+    if(!is.null(cumulative)){
+        if (!is.logical(cumulative)){
+            stop("cumulative must be in Boolean format")
+        }else{
+            q <- c(q, paste0("cumulative=", tolower(cumulative)), "&")
         }
-        if(class(list_obj) == "data.frame"){
-            return(list_obj)
+    }
+    if(!is.null(subadmin)){
+        if (!is.logical(subadmin)){
+            stop("subadmin must be in Boolean format")
+        }else{
+            q <- c(q, paste0("subadmin=", tolower(subadmin)), "&")
         }
-        ## If list add a "query_key" column
-        query_keys <- names(list_obj)
-        res <- lapply(query_keys,
-                      function(query_key) {
-                          d <- list_obj[[query_key]]
-                          d$query_key <- query_key
-                          return(d)
-                      })
-        res_df <- do.call(rbind, out)
-        return(res_df);
+    }
+    if(!is.null(pangolin_lineage)){
+        q <- c(q, paste0("pangolin_lineage=", pangolin_lineage, "&"))
+    }
+    if(!is.null(mutations)){
+        mutations <- paste(mutations, collapse=",")
+        q <- c(q, paste0("mutations=", mutations, "&"))
+    }
+    if(!is.null(ndays)){
+        q <- c(q, paste0("ndays=", ndays, "&"))
+    }
+    if(!is.null(frequency)){
+        q <- c(q, paste0("frequency=", frequency, "&"))
+    }
+    if(!is.null(other_threshold)){
+        q <- c(q, paste0("other_threshold=", other_threshold, "&"))
+    }
+    if(!is.null(nday_threshold)){
+        q <- c(q, paste0("nday_threshold=", nday_threshold, "&"))
+    }
+    if(!is.null(other_exclude)){
+        other_exclude <- paste(other_exclude, collapse=",")
+        q <- c(q, paste0("other_exclude=", other_exclude, "&"))
     }
 
-  genomic_url <- "https://dev.outbreak.info/genomics/"
+    q <- paste(q, sep="", collapse = "")
+    q <- sub("&$", "", q)
 
-  q <- c()
+    dataurl <- paste0(genomic_url, q)
+    results <- getGenomicsResponse(dataurl);
 
-  q <- c(q, paste0(query_url), "?")
-
-  if(!is.null(location)){
-    location <- getISO3_genomic(location)
-    q <- c(q, paste0("location_id=", location, "&"))
-  }
-  if(!is.null(cumulative)){
-    if (!is.logical(cumulative)){
-      stop("cumulative must be in Boolean format")
+    if (length(results) > 1){
+        hits <- rbind_pages(results)
     }else{
-      q <- c(q, paste0("cumulative=", tolower(cumulative)), "&")
+        hits <- data.frame(results)
     }
-  }
-  if(!is.null(subadmin)){
-    if (!is.logical(subadmin)){
-      stop("subadmin must be in Boolean format")
-    }else{
-      q <- c(q, paste0("subadmin=", tolower(subadmin)), "&")
+    if ("date" %in% colnames(hits)){
+        hits$date=as.Date(hits$date, "%Y-%m-%d")
+        hits <- hits[order(as.Date(hits$date, format = "%Y-%m-%d")),]
     }
-  }
-  if(!is.null(pangolin_lineage)){
-    q <- c(q, paste0("pangolin_lineage=", pangolin_lineage, "&"))
-  }
-  if(!is.null(mutations)){
-    mutations <- paste(mutations, collapse=",")
-    q <- c(q, paste0("mutations=", mutations, "&"))
-  }
-  if(!is.null(ndays)){
-    q <- c(q, paste0("ndays=", ndays, "&"))
-  }
-  if(!is.null(frequency)){
-    q <- c(q, paste0("frequency=", frequency, "&"))
-  }
-  if(!is.null(other_threshold)){
-    q <- c(q, paste0("other_threshold=", other_threshold, "&"))
-  }
-  if(!is.null(nday_threshold)){
-    q <- c(q, paste0("nday_threshold=", nday_threshold, "&"))
-  }
-  if(!is.null(other_exclude)){
-    other_exclude <- paste(other_exclude, collapse=",")
-    q <- c(q, paste0("other_exclude=", other_exclude, "&"))
-  }
-
-  q <- paste(q, sep="", collapse = "")
-  q <- sub("&$", "", q)
-
-  dataurl <- paste0(genomic_url, q)
-
-  scroll.id <- NULL
-  results <- list()
-  success <- NULL
-  while(is.null(success)){
-      success <- FALSE
-    cat("Retrieving data...", "\n")
-      dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-      dataurl <- URLencode(dataurl)
-      print(dataurl)
-      resp <- NULL
-      tryCatch({
-          if(Sys.getenv("OUTBREAK_INFO_TOKEN") != ""){
-              resp <- GET(
-                  dataurl,
-                  add_headers(Authorization = paste("Bearer", Sys.getenv("OUTBREAK_INFO_TOKEN"), sep=" "))
-              )
-          } else {
-              resp <- GET(
-                  dataurl
-              )
-          }
-        auth_token = resp$headers$`x-auth-token`
-        if(!is.null(auth_token))
-            Sys.setenv(OUTBREAK_INFO_TOKEN = auth_token)
-        if(resp$status_code == 401){
-            warning("Please authenticateUser by calling authenticateUser() to access the API.")
-        } else if (resp$status_code == 403) {
-            warning("Invalid taken. Please reauthenticateUser by calling the authenticateUser() function.")
-        } else if(resp$status_code == 500){
-            warning("There was an internal server error. Please cross check your query or contact help@outbreak.info for further assistance.")
-        } else if(resp$status_code == 429){
-            warning("You have exceeded the API usage limit. Please limit the usage to 1 request/minute.")
-        } else if (resp$status_code == 400){
-            warning("Malformed token. Please reauthenticateUser by calling the authenticateUser() function.")
-        } else if(resp$status_code == 200){
-            resp <- fromJSON(content(resp, "text"), flatten=TRUE)
-            resp_df <- convert_list_to_dataframe(resp$results)
-            results[[length(results) + 1]] <- resp_df
-            scroll.id <- resp$'_scroll_id'
-            success <- resp$success
-        }
-    }, error = function(cond){
-        stop("Could not connect to API. Please check internet connection and try again.");
-    }, warning = function(cond){
-        message(cond)
-    })
-  }
-  if (length(results) > 1){
-    hits <- rbind_pages(results)
-  }else{
-    hits <- data.frame(results)
-  }
-  if ("date" %in% colnames(hits)){
-    hits$date=as.Date(hits$date, "%Y-%m-%d")
-    hits <- hits[order(as.Date(hits$date, format = "%Y-%m-%d")),]
-  }
-  return(hits)
+    return(hits)
 }
-
-  df <- getGenomicData(query_url="prevalence-by-location-all-lineages", location = "United States", other_threshold = 0.03, nday_threshold = 5, ndays = 60)
--- outbreakinfo/R/getISO3_genomic.R
+++ outbreakinfo/R/getISO3_genomic.R
@@ -13,111 +13,74 @@
 #' @import jsonlite
 #' @import plyr
 
-
 getISO3_genomic <- function(locations_to_search){
-  loc_url <- "https://api.outbreak.info/genomics/location?"
-  locs_of_interest=c()
-  locs_not_found=c()
-  for (i in locations_to_search){
-    scroll.id <- NULL
-    location.ids <- paste0("name=", paste(i))
-    location.ids <- gsub("&", "%26", location.ids)
-    results <- list()
-    success <- NULL
-    while(is.null(success)){
-      dataurl <- paste0(loc_url, location.ids)
-      dataurl <- gsub(" ", "+", dataurl)
-      dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-      t <- try(fromJSON(dataurl, flatten=TRUE), silent=T)
-      if(grepl("Error in open.connection(con, \"rb\")", t[1], fixed=T)){
-        stop("Could not connect to API. Check internet connection and try again.")
-      }else{
-        resp <- fromJSON(dataurl, flatten=TRUE)
-        results[[length(results) + 1]] <- resp$results
-        scroll.id <- resp$'_scroll_id'
-        success <- resp$success
-      }
-    }
-    t <- try(rbind_pages(results), silent=T)
-    if("try-error" %in% class(t)){
-      error=T
-      locs_not_found = c(locs_not_found, i)
-    }else{
-      hits <- rbind_pages(results)
-      df=(hits)
-      if (nrow(df)==1){
-        locs_of_interest=c(locs_of_interest, df$id)
-      }else{
-        locs_not_found=c(locs_not_found, i)
-      }
-    }
-  }
-  if (length(locs_of_interest)==length(locations_to_search)){
-    return(locs_of_interest)
-  }
-  if (length(locs_of_interest)!=length(locations_to_search)){
-    locations=c()
-    for (i in locs_not_found){
-      if (grepl(" ", i, fixed=TRUE)==T){
-        locs=paste0("*",i,"*")
-        locs=gsub(" ", "*", locs, fixed=TRUE)
-      }else{
-        locs=paste0("*",i,"*")
-      }
-      locations=c(locations, locs)
-    }
-    for (i in 1:length(locations)){
-      scroll.id <- NULL
-      location.ids <- paste0("name=", paste(locations[i]))
-      location.ids <- gsub("&", "%26", location.ids)
-      results <- list()
-      success <- NULL
-      while(is.null(success)){
+    loc_url <- "https://dev.outbreak.info/genomics/location?"
+    locs_of_interest=c()
+    locs_not_found=c()
+    for (i in locations_to_search){
+        location.ids <- paste0("name=", paste(i))
         dataurl <- paste0(loc_url, location.ids)
-        dataurl <- gsub(" ", "+", dataurl)
-        dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-        t <- try(fromJSON(dataurl, flatten=TRUE), silent=T)
-        if(grepl("Error in open.connection(con, \"rb\")", t[1], fixed=T)){
-          stop("Could not connect to API. Check internet connection and try again.")
-        }else{
-          resp <- fromJSON(dataurl, flatten=TRUE)
-          results[[length(results) + 1]] <- resp$results
-          scroll.id <- resp$'_scroll_id'
-          success <- resp$success
-        }
-      }
-      t2 <- try(rbind_pages(results), silent=T)
-      if("try-error" %in% class(t2)){
-        message(paste(locs_not_found[i], " not found. Please check spelling."))
-        next
-      }else{
+        results <- getGenomicsResponse(dataurl)
         hits <- rbind_pages(results)
-        df=(hits)
-        df$admin_level[df$admin_level == "-1"] <- "World Bank Region"
-        df$admin_level[df$admin_level == "0"] <- "country"
-        df$admin_level[df$admin_level == "1"] <- "state/province"
-        df$admin_level[df$admin_level == "1.5"] <- "metropolitan area"
-        df$admin_level[df$admin_level == "2"] <- "county"
-        df$full <- paste0(df$label, " (", df$admin_level, ")")
-      }
-      for (i in df$full){
-        cat(i)
-        loc_sel <- readline("Is this a location of interest? (Y/N): ")
-        if ((loc_sel == "Y")|(loc_sel == "y")){
-          locs_of_interest = c(locs_of_interest, df$id[df$full==i])
-          break
+        if(nrow(hits) == 0){
+            locs_not_found = c(locs_not_found, i)
+        } else {
+            hits <- rbind_pages(results)
+            df=(hits)
+            if (nrow(df)==1){
+                locs_of_interest=c(locs_of_interest, df$id)
+            }else{
+                locs_not_found=c(locs_not_found, i)
+            }
         }
-        if ((loc_sel != "Y")&(loc_sel != "y")&(loc_sel != "N")&(loc_sel != "n")){
-          cat("Expected input is Y or N\n\n")
-          cat(i)
-          loc_sel <- readline("Is this a location of interest? (Y/N): ")
-          if ((loc_sel == "Y")|(loc_sel == "y")){
-            locs_of_interest = c(locs_of_interest, df$id[df$full==i])
-            break
-          }
-        }
-      }
     }
-  }
-  return(locs_of_interest)
+    if (length(locs_of_interest)==length(locations_to_search)){
+        return(locs_of_interest)
+    }
+    if (length(locs_of_interest)!=length(locations_to_search)){
+        locations=c()
+        for (i in locs_not_found){
+            if (grepl(" ", i, fixed=TRUE)==T){
+                locs=paste0("*",i,"*")
+            }else{
+                locs=paste0("*",i,"*")
+            }
+            locations=c(locations, locs)
+        }
+        for (i in 1:length(locations)){
+            location.ids <- paste0("name=", paste(locations[i]))
+            dataurl <- paste0(loc_url, location.ids)
+            results <- getGenomicsResponse(dataurl)
+            hits <- rbind_pages(results)
+            if(nrow(hits) == 0){
+                next
+            } else {
+                df=(hits)
+                df$admin_level[df$admin_level == "-1"] <- "World Bank Region"
+                df$admin_level[df$admin_level == "0"] <- "country"
+                df$admin_level[df$admin_level == "1"] <- "state/province"
+                df$admin_level[df$admin_level == "1.5"] <- "metropolitan area"
+                df$admin_level[df$admin_level == "2"] <- "county"
+                df$full <- paste0(df$label, " (", df$admin_level, ")")
+                for (i in df$full){
+                    cat(i)
+                    loc_sel <- readline("Is this a location of interest? (Y/N): ")
+                    if ((loc_sel == "Y")|(loc_sel == "y")){
+                        locs_of_interest = c(locs_of_interest, df$id[df$full==i])
+                        break
+                    }
+                    if ((loc_sel != "Y")&(loc_sel != "y")&(loc_sel != "N")&(loc_sel != "n")){
+                        cat("Expected input is Y or N\n\n")
+                        cat(i)
+                        loc_sel <- readline("Is this a location of interest? (Y/N): ")
+                        if ((loc_sel == "Y")|(loc_sel == "y")){
+                            locs_of_interest = c(locs_of_interest, df$id[df$full==i])
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return(locs_of_interest)
 }

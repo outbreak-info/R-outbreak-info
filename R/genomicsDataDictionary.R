fa3020e9a3308afa55365e119c90ce7a8b793c7e
flaneuse <flaneuse@users.noreply.github.com>
2021-10-08 18:29:05 -0700

    fix dict typo

--- R/genomicsDataDictionary.R
+++ R/genomicsDataDictionary.R
@@ -16,9 +16,9 @@ genomicsDataDictionary <- function() {
                  "lineage_count_rolling", "7 day rolling average of samples assigned to the Pango lineage on that day",
                  "prevalence", "Estimated prevalence of the variant on that day",
                  "prevalence_rolling", "7 day rolling average estimate of prevalence of the variant on that day",
-                 "proportion": "Estimated prevalence of a variant: lineage_count_rolling / total_count_rolling",
-                 "proportion_ci_lower": "95% confidence interval lower bound of estimated prevalence of a variant, calculated using Jeffrey's interval",
-                 "proportion_ci_upper": "95% confidence interval upper bound of estimated prevalence of a variant, calculated using Jeffrey's interval",
+                 "proportion", "Estimated prevalence of a variant: lineage_count_rolling / total_count_rolling",
+                 "proportion_ci_lower", "95% confidence interval lower bound of estimated prevalence of a variant, calculated using Jeffrey's interval",
+                 "proportion_ci_upper", "95% confidence interval upper bound of estimated prevalence of a variant, calculated using Jeffrey's interval",
                  "total_count", "Total number of samples sequenced on a given day",
                  "total_count_rolling", "7 day rolling average of total samples sequenced on that day"
                  )

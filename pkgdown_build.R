f5f58999624daa5736283b6ad7dd76150fd57529
flaneuse <flaneuse@users.noreply.github.com>
2021-10-08 19:58:32 -0700

    add auth token as a param

--- pkgdown_build.R
+++ pkgdown_build.R
@@ -4,16 +4,19 @@
 # NOTE: you'll need to supervise this process, because every example that
 # contains a call to a genomics function will require GISAID authentication.
 
-update_package_website = function(commit_message = "update R outbreakinfo package website"){
-# 1. Knit the README.Rmd file
-# Creates README.md
-knitr::knit("README.Rmd")
+update_package_website = function(auth_token, commit_message = "update R outbreakinfo package website"){
+  # Set the authentication token
+  Sys.setenv(OUTBREAK_INFO_TOKEN = auth_token)
 
-# 2. Build the site
-# Note: some of the functions in the examples take a fair bit of time to run.
-pkgdown::build_site()
+  # 1. Knit the README.Rmd file
+  # Creates README.md
+  knitr::knit("README.Rmd")
 
-# 3. Push to GitHub; will automatically update GitHub pages.
-gert::git_commit_all(commit_message)
-gert::git_push()
+  # 2. Build the site
+  # Note: some of the functions in the examples take a fair bit of time to run.
+  pkgdown::build_site()
+
+  # 3. Push to GitHub; will automatically update GitHub pages.
+  gert::git_commit_all(commit_message)
+  gert::git_push()
 }

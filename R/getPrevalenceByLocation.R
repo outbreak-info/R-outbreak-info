856cb9c938b47499efab551d8cb86cb5a4015a00
flaneuse <flaneuse@users.noreply.github.com>
2021-10-10 16:12:46 -0700

    Clarify how to get mutation-only reports
    Closes #50

--- R/getPrevalenceByLocation.R
+++ R/getPrevalenceByLocation.R
@@ -2,9 +2,9 @@
 #'
 #' @description Retrieves the daily prevalence of a PANGO lineage(s) by location
 #'
-#' @param pangolin_lineage: PANGO lineage name or list of PANGO lineage names. A list of lineages will return a long dataframe with
+#' @param pangolin_lineage: (optional) PANGO lineage name or vector of PANGO lineage names. Either `pangolin_lineage` or `mutations` needs to be specified. A list of lineages will return a long dataframe with `lineage` as a variable; if you want to calculate the prevalence of lineage1 or lineage2 together, enter the lineages separated by " OR ". For instance, to calculate the prevalence of Delta, you'll need to supply `"B.1.617.2 OR AY.1 OR AY.2 OR ..."` **Be sure to include the space around "OR" and it must be capitalized.**
+#' @param mutations: (optional) a `vector` of mutation(s). Either `pangolin_lineage` or `mutations` needs to be specified.
 #' @param location: (optional) a location name
-#' @param mutations: (optional) a `vector` of mutation(s)
 #' @param cumulative: (optional) `Boolean` (T/F), T returns cumulative prevalence since first day of detection
 #'
 #' @return dataframe
@@ -13,13 +13,14 @@
 #' # lineage: P.1 in Brazil
 #' getPrevalenceByLocation(pangolin_lineage = "P.1", location = "Brazil") %>% head()
 #'
-#' # AY.4 and B.1.617.2 in Brazil (AY.4 and B.1.617.2 separately)
-#' getPrevalenceByLocation(pangolin_lineage = c("AY.4", "B.1.617.2"), location = "Brazil") %>% filter(date == "2021-08-01")
+#' # AY.4 and B.1.617.2 in Brazil (AY.4, AY.34, and B.1.617.2 separately)
+#' getPrevalenceByLocation(pangolin_lineage = c("AY.4", "AY.34", "B.1.617.2"), location = "Brazil") %>% filter(date == "2021-09-01")
 #'
-#' # AY.4 OR B.1.617.2 in Brazil (AY.4 and B.1.617.2 combined)
-#' getPrevalenceByLocation(pangolin_lineage = "AY.4 OR B.1.617.2", location = "Brazil") %>% filter(date == "2021-08-01")
+#' # AY.4 OR B.1.617.2 in Brazil (AY.4, AY.34 and B.1.617.2 combined)
+#' getPrevalenceByLocation(pangolin_lineage = "AY.4 OR AY.34 OR B.1.617.2", location = "Brazil") %>% filter(date == "2021-09-01")
 #'
-#' # S:E484K mutation
+#' # S:E484K mutation prevalence worldwide
+#' getPrevalenceByLocation(mutations = c("S:E484K"))
 #'
 #' # B.1.1.7 + S:E484K mutation worldwide
 #' getPrevalenceByLocation(pangolin_lineage = "B.1.1.7", mutations = c("S:E484K"))
@@ -27,7 +28,11 @@
 
 
 
-getPrevalenceByLocation <- function(pangolin_lineage, location=NULL, mutations=NULL, cumulative=NULL){
+getPrevalenceByLocation <- function(pangolin_lineage=NULL, location=NULL, mutations=NULL, cumulative=NULL){
+  if(is.null(pangolin_lineage) && is.null(mutations)) {
+    stop("Either `pangolin_lineage` or `mutations` needs to be specified")
+  }
+
   if(is.vector(pangolin_lineage)) {
     lineage_string = paste(pangolin_lineage, collapse = ",")
   } else {

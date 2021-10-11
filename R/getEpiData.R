96687dfc181f07b7ac439111be60046fa8775a22
gkarthik <xraydiffraction@gmail.com>
2021-10-10 22:58:13 -0700

    Added url encoding and restructured GET request

--- R/getEpiData.R
+++ R/getEpiData.R
@@ -24,41 +24,40 @@
 getEpiData <- function(name=NULL, location_id=NULL, wb_region=NULL, country_name=NULL, state_name=NULL, admin_level=NULL, date=NULL, mostRecent=NULL, fields=NULL, sort=NULL, size=1000){
   q <- c()
   if(!is.null(name)){
-    q <- c(q, paste0("(name:%22", paste(name, collapse="%22%20OR%20name:%22"), "%22)%20AND%20"))
+    q <- c(q, paste0("(name:\"", paste(name, collapse="\" OR name:\""), "\") AND "))
   }
   if(!is.null(location_id)){
-    q <- c(q, paste0("(location_id:%22", paste(location_id, collapse="%22%20OR%20location_id:%22"), "%22)%20AND%20"))
+    q <- c(q, paste0("(location_id:\"", paste(location_id, collapse="\" OR location_id:\""), "\") AND "))
   }
   if(!is.null(wb_region)){
-    q <- c(q, paste0("(wb_region:%22", paste(wb_region, collapse="%22%20OR%20wb_region:%22"), "%22)%20AND%20"))
+    q <- c(q, paste0("(wb_region:\"", paste(wb_region, collapse="\" OR wb_region:\""), "\") AND "))
   }
   if(!is.null(country_name)){
-    q <- c(q, paste0("(country_name:%22", paste(country_name, collapse="%22%20OR%20country_name:%22"), "%22)%20AND%20"))
+    q <- c(q, paste0("(country_name:\"", paste(country_name, collapse="\" OR country_name:\""), "\") AND "))
   }
   if(!is.null(state_name)){
-    q <- c(q, paste0("(state_name:%22", paste(state_name, collapse="%22%20OR%20state_name:%22"), "%22)%20AND%20"))
+    q <- c(q, paste0("(state_name:\"", paste(state_name, collapse="\" OR state_name:\""), "\") AND "))
   }
   if(!is.null(admin_level)){
-    q <- c(q, paste0("(admin_level:%22", paste(admin_level, collapse="%22%20OR%20admin_level:%22"), "%22)%20AND%20"))
+    q <- c(q, paste0("(admin_level:\"", paste(admin_level, collapse="\" OR admin_level:\""), "\") AND "))
   }
   if(!is.null(date)){
     if (!is.character(date)){
       stop("Date must be in string format")
     }else{
-      q <- c(q, paste0("(date:%22", paste(date, collapse="%22%20OR%20date:%22"), "%22)%20AND%20"))
+      q <- c(q, paste0("(date:\"", paste(date, collapse="\" OR date:\""), "\") AND "))
     }
   }
   q <- paste(q, sep="", collapse = "")
-  q <- substr(q, 1, nchar(q)-9)
+  q <- substr(q, 1, nchar(q)-5)
   if(!is.null(mostRecent)){
     if (!is.logical(mostRecent)){
       stop("mostRecent must be in Boolean format")
     }else{
-      q <- c(q, paste0("%20AND%20", "mostRecent:", tolower(mostRecent)))
+      q <- c(q, paste0(" AND ", "mostRecent:", tolower(mostRecent)))
     }
   }
   q <- paste(q, sep="", collapse = "")
-  q <- gsub("&", "%26", q)
   if(!is.null(fields)){
     q <- c(q, paste0("&fields=", paste(fields, collapse=",")))
   }
@@ -69,17 +68,19 @@ getEpiData <- function(name=NULL, location_id=NULL, wb_region=NULL, country_name
   q <- c(q, paste0("&size=", paste(size)))
   q <- paste(q, sep="", collapse = "")
   q <- paste0(q, "&fetch_all=true")
-  q <- gsub(" ", "+", q)
 
   dataurl <- paste0(api.url, "query?q=", q)
-  t <- try(fromJSON(dataurl, flatten=TRUE), silent=T)
-  if(grepl("Error in open.connection(con, \"rb\")", t[1], fixed=T)){
-    stop("Could not connect to API. Check internet connection and try again.")
-  }else{
-    resp <- fromJSON(dataurl, flatten=TRUE)
+  dataurl <- URLencode(dataurl)
+  print(dataurl)
+  resp <- GET(
+      dataurl
+  )
+  if(resp$status_code == 200){
+      resp <- fromJSON(content(resp, "text"), flatten=TRUE)
+  } else {
+      stop("Could not connect to API. Check internet connection and try again. If the problem persists please contact help@outbreak.info.")
   }
   max <- resp$total
-
   scroll.id <- NULL
   results <- list()
   success <- NULL
@@ -90,19 +91,23 @@ getEpiData <- function(name=NULL, location_id=NULL, wb_region=NULL, country_name
   while(is.null(success)){
     dataurl <- paste0(api.url, "query?q=", q)
     dataurl <- ifelse(is.null(scroll.id), dataurl, paste0(dataurl, "&scroll_id=", scroll.id))
-    t <- try(fromJSON(dataurl, flatten=TRUE), silent=T)
-    if(grepl("Error in open.connection(con, \"rb\")", t[1], fixed=T)){
-      stop("Could not connect to API. Check internet connection and try again.")
-    }else{
-      resp <- fromJSON(dataurl, flatten=TRUE)
-      scroll.id <- resp$'_scroll_id'
-      results[[length(results) + 1]] <- resp$hits
-      success <- resp$success
-      if (is.null(success)){
+    dataurl <- URLencode(dataurl)
+    resp <- GET(
+        dataurl
+    )
+    if(resp$status_code == 200){
+        resp <- fromJSON(content(resp, "text"), flatten=TRUE)
+    } else {
+        stop("Could not connect to API. Check internet connection and try again. If the problem persists please contact help@outbreak.info.")
+    }
+    resp <- fromJSON(dataurl, flatten=TRUE)
+    scroll.id <- resp$'_scroll_id'
+    results[[length(results) + 1]] <- resp$hits
+    success <- resp$success
+    if (is.null(success)){
         pb$tick(size)
-      }else{
+    } else {
         pb$finished <- T
-      }
     }
   }
   pb$terminate()

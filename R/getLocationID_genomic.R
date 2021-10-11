0fca2c065535c1392bef07843ead52a49fc52633
gkarthik <xraydiffraction@gmail.com>
2021-10-10 18:46:14 -0700

    Log warnings only one first request

--- R/getLocationID_genomic.R
+++ R/getLocationID_genomic.R
@@ -20,7 +20,7 @@ getLocationID_genomic <- function(locations_to_search){
     for (i in locations_to_search){
         location.ids <- paste0("name=", paste(i))
         dataurl <- paste0(loc_url, location.ids)
-        results <- getGenomicsResponse(dataurl, F)
+        results <- getGenomicsResponse(dataurl, logInfo = F, logWarning = T, logError = T)
         hits <- data.frame()
         if(length(results) >= 1)
             hits <- rbind_pages(results)
@@ -52,7 +52,7 @@ getLocationID_genomic <- function(locations_to_search){
         for (i in 1:length(locations)){
             location.ids <- paste0("name=", paste(locations[i]))
             dataurl <- paste0(loc_url, location.ids)
-            results <- getGenomicsResponse(dataurl, F)
+            results <- getGenomicsResponse(dataurl, F, logWarning = F, logError = T)
             hits <- data.frame()
             if(length(results) >= 1)
                 hits <- rbind_pages(results)

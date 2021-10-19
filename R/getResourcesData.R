ea40e3cef6779e092377aa37e7f3474cdf20c8f3
flaneuse <flaneuse@users.noreply.github.com>
2021-10-18 19:09:06 -0700

    updated resources documentation / vignettes

--- R/getResourcesData.R
+++ R/getResourcesData.R
@@ -1,7 +1,7 @@
 #' Get COVID-19 resource metadata
 #'
-#' @param query (optional): constructs a query over ALL fields, or a fielded query searching within specific fields. Fielded query terms should be separated by ` AND ` or ` OR `. See Elasticserach query strings for more info.
-#' @param types (optional): vector of resource type 
+#' @param query (optional): constructs a query over ALL fields, or a fielded query searching within specific fields. Fielded query terms should be separated by ` AND ` or ` OR `. See \href{https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#query-string-syntax}{Elasticserach query strings} for more info.
+#' @param types (optional): vector of resource type to return. The most frequent types include: "Publication", "ClinicalTrial", "Dataset", "Protocol", "SoftwareSourceCode", "Analysis".
 #' @param size (optional): number of records to return (default = 10)
 #' @param fetchAll (optional): Boolean whether to return all results for the query
 #' @param fields (optional): vector specifying which fields to return. Returns all by default
@@ -17,15 +17,23 @@
 #' @examples
 #' # Get the date latest date for every resource (newest of dateModified, dateCreated, datePublished)
 #' resources_by_date = getResourcesData(query = "date:[2020-01-01 TO *]", fields = "date")
+#' 
+#' # Get all metadata on remdesivir
+#' remdesivir = getResourcesData(query = "remdesivir", fetchAll = TRUE, fields = c("@type", "name", "curatedBy"))
+#' remdesivir %>% count(`@type`) %>% arrange(desc(n))
+#' 
+#' # Get all metadata for remdesivir Clinical Trials or Datasets
+#' remdesivir_trials_data = getResourcesData(query = "remdesivir", types = c("ClinicalTrial", "Dataset"), fetchAll = TRUE, fields = c("@type", "name", "curatedBy"))
+#' remdesivir_trials_data %>% count(`@type`) %>% arrange(desc(n))
 
-getResourcesData = function(query = NULL, types = NULL, size = 10, fetchAll = FALSE, fields = null, sort = NULL, facets = NULL, facet_size = 10) {
+getResourcesData = function(query = NULL, types = NULL, size = 10, fetchAll = FALSE, fields = NULL, sort = NULL, facets = NULL, facet_size = 10) {
   
   # Append the `@type: filter` if selected
   if(!is.null(types)) {
     if(!is.null(query)) {
-      query = str_c(" AND @type=", paste(types, sep = " OR "))
+      query = str_c(query, " AND @type:(", paste(types, collapse = " OR "), ")")
     } else {
-      query = str_c("@type=", paste(types, sep = " OR "))
+      query = str_c("@type:(", paste(types, collapse = " OR "), ")")
     }
   }
   
--- R/getResourcesResponse.R
+++ R/getResourcesResponse.R
@@ -1,6 +1,6 @@
 #' Access outbreak.info Research Library API
 #'
-#' @param queryString (optional): constructs a query over ALL fields, or a fielded query searching within specific fields. Fielded query terms should be separated by ` AND ` or ` OR `
+#' @param query (optional): constructs a query over ALL fields, or a fielded query searching within specific fields. Fielded query terms should be separated by ` AND ` or ` OR `. See \href{https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#query-string-syntax}{Elasticserach query strings} for more info.
 #' @param size (optional): number of records to return (default = 10)
 #' @param fetchAll (optional): Boolean whether to return all results for the query
 #' @param fields (optional): vector specifying which fields to return. Returns all by default
@@ -14,57 +14,61 @@
 #' @import stringr
 #' @import dplyr
 #'
-#' @return dataframe containing the response of the API query
+#' @return dataframe or list (for facetted queries) containing the response of the API query
 #' @export
 #'
 #' @examples
-#' # Return only the first 10 hits for
+#' # Return only the first 10 hits for "ivermectin"
 #' df = getResourcesResponse("ivermectin", size = 10)
+#' 
+#' # Find the first 10 results which contain the variable `topicCategory`  
 #' df = getResourcesResponse("_exists_:topicCategory", size = 10)
+#' 
+#' # Get `date`/`name` from the first 10 hits for the query "ivermectin" plus a count of the `@type` field for ALL results
 #' df = getResourcesResponse("ivermectin", size = 10, facets="@type", fields=c("date", "name"))
 
-getResourcesResponse = function(queryString = NULL, size = 10, fetchAll = FALSE, fields = NULL, sort = NULL, facets = NULL, facet_size = 10) {
-
+getResourcesResponse = function(queryString = NULL, size = 10, fetchAll = FALSE, fields = NULL, sort = NULL, facets = NULL, facet_size = 10, queryStub = "https://api.outbreak.info/resources/query?") {
+  
   # Construct the query
-  query = "https://api.outbreak.info/resources/query?"
-
+  query = queryStub
+  
   # add base query
   if(!is.null(queryString)){
-    query = str_c("https://api.outbreak.info/resources/query?q=", queryString)
+    query = str_c(queryStub, queryString)
   }
-
-
+  
+  
   # add sorting
   if(!is.null(sort)){
     query = str_c(query, "&sort=", sort)
   }
-
+  
   # add size param
   if(!is.null(size)){
     query = str_c(query, "&size=", size)
   }
-
+  
   # add fields param
   if(!is.null(fields)){
-    query = str_c(query, "&fields=", paste(fields, sep=","))
+    query = str_c(query, "&fields=", paste(fields, collapse=","))
   }
-
+  
   # add facets param
   if(!is.null(facets)){
     query = str_c(query, "&facets=", facets, "&facet_size=", facet_size)
   }
-
+  
   # --- RUN THE QUERY(-IES) --
   if(fetchAll) {
     # Fetch all queries -- keep going till you hit the end
     query = str_c(query, "&fetch_all=true")
-
+    
     df = tibble()
     res = getResourcesQuery(query)
-
+    
     # add progress bar
     pb <- progress_bar$new(total = res$total, clear = FALSE, show_after = 0)
-
+    
     while(!is.null(res)){
       pb$update(nrow(df) / res$total)
       df = df %>% bind_rows(res$hits)
@@ -72,59 +76,61 @@ getResourcesResponse = function(queryString = NULL, size = 10, fetchAll = FALSE,
     }
     pb$update(1)
     pb$terminate()
-
+    
     # return statement
     if(!is.null(facets)) {
       return(list(hits = df, total = res[["total"]], facets = res[["facets"]]))
     } else {
       return(df)
     }
-
-
+    
+    
   } else {
     # Single query
     results = getResourcesQuery(query)
   }
-
+  
   # Data cleanup!
   # remove `_score`
-  results$hits = results$hits %>%
-    select(-`_score`)
-
+  if(!is.null(results$hits)){
+    results$hits = results$hits %>%
+      select(-`_score`)
+  }
+  
   if("date" %in% colnames(results$hits)) {
     results$hits = results$hits %>%
       mutate(date =  as.Date(date, "%Y-%m-%d"))
   }
-
+  
   if("datePublished" %in% colnames(results$hits)) {
     results$hits = results$hits %>%
       mutate(datePublished =  as.Date(datePublished, "%Y-%m-%d"))
   }
-
+  
   if("dateModified" %in% colnames(results$hits)) {
     results$hits = results$hits %>%
       mutate(dateModified =  as.Date(dateModified, "%Y-%m-%d"))
   }
-
-
+  
+  
   if("dateCreated" %in% colnames(results$hits)) {
     results$hits = results$hits %>%
       mutate(dateCreated =  as.Date(dateCreated, "%Y-%m-%d"))
   }
-
-
+  
+  
   if("dateCompleted" %in% colnames(results$hits)) {
     results$hits = results$hits %>%
       mutate(dateCompleted =  as.Date(dateCompleted, "%Y-%m-%d"))
   }
-
+  
   # Flatten the results, if they don't include an aggregation
   if(!is.null(facets)) {
     return(results)
   } else {
     return(results$hits)
   }
-
+  
 }
 
 # helper function to grab the results
@@ -133,7 +139,7 @@ getResourcesQuery = function(query, scroll_id = NA) {
     query = str_c(query, "&scroll_id=", scroll_id)
   }
   resp = GET(URLencode(query))
-
+  
   if(resp$status_code == 200) {
     resp_content = content(resp, as="text")
     results = fromJSON(resp_content)

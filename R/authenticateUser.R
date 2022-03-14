34ec4728746f20cba8be61e3cb285f9280df12e3
gkarthik <xraydiffraction@gmail.com>
2022-03-14 15:56:29 -0700

    Incorporated changes to terms

--- R/authenticateUser.R
+++ R/authenticateUser.R
@@ -54,20 +54,16 @@ authenticateUser <- function(){
 printTerms <- function(){
     termsText <- "
 NOTICE AND REMINDER of TERMS OF USE:
-The Terms of Use you agreed to when requesting access credentials to GISAID include the following:
 
-1) You will not distribute data made available through GISAID (“Data in GISAID”) with others who have not agreed to the GISAID Terms of Use, and
-2) You will not display the data in any form on a website without additional express written permission from GISAID, and
-3) You will treat all data contained in these files consistent with other Data in GISAID and in accordance with the GISAID Terms of Use, and
-4) You will provide proper attributions and acknowledgements consistent with the GISAID Terms of Use when using Data in GISAID in publications, manuscripts, and any other analyses.
+The Terms of Use you agreed to when requesting access credentials to GISAID include the following terms:
+1.	You will not distribute data made available through GISAID (“Data in GISAID”) to any third party other than Authorized Users as contemplated by GISAID's Database Access Agreement;
+2.	You will not display the data in any form in a publication, preprint, manuscript, or any other media material including on a website, without additional express written permission from GISAID;
+3.	You will treat all data contained in these files consistent with other Data in GISAID and in accordance with GISAID's Database Access Agreement; and
+4.	You will provide proper attributions, acknowledgements, and efforts to collaborate with data submitters consistent with the GISAID's and Database Access Agreement when using Data in GISAID in publications, preprints, manuscripts, or any other analyses or media material.
 
-You can see the full terms of use at https://www.gisaid.org/registration/terms-of-use/.
+You can see the full terms of use at https://www.gisaid.org/registration/terms-of-use/. By using the outbreak.info R package you reaffirm your understanding of these terms and the DAA.
 
-By using the outbreakinfo R package you reaffirm your understanding of these terms and the DAA.\n\n
-
-When using this data, please state, \"This Data was obtained from GISAID via the outbreak.info API\"\n
-WE DO NOT SUPPORT THIRD PARTY APPLICATIONS. THIS PACKAGE IS MEANT FOR RESEARCH AND VISUALIZATION PURPOSES ONLY.\n
-If you want to build third party applications please contact GISAID via https://www.gisaid.org/help/contact/.\n
+When using this data, please state, \"This data was obtained from GISAID via the outbreak.info API\". WE DO NOT SUPPORT THIRD PARTY APPLICATIONS. THIS PACKAGE IS MEANT FOR RESEARCH AND VISUALIZATION PURPOSES ONLY. If you want to build third party applications, please contact GISAID via https://www.gisaid.org/help/contact/.
 "
     cat(termsText)
 }

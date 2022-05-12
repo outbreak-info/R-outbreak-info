e3907bd4fcf60a08c7fdf4c3a79d4cf4f23169ed
gkarthik <xraydiffraction@gmail.com>
2022-05-12 15:59:21 -0700

    Updated terms and conditions

--- R/authenticateUser.R
+++ R/authenticateUser.R
@@ -53,17 +53,42 @@ authenticateUser <- function(){
 
 printTerms <- function(){
     termsText <- "
-NOTICE AND REMINDER of TERMS OF USE:
+TERMS OF USE for R Package and
 
-The Terms of Use you agreed to when requesting access credentials to GISAID include the following terms:
-1.	You will not distribute data made available through GISAID (“Data in GISAID”) to any third party other than Authorized Users as contemplated by GISAID's Database Access Agreement;
-2.	You will not display the data in any form in a publication, preprint, manuscript, or any other media material including on a website, without additional express written permission from GISAID;
-3.	You will treat all data contained in these files consistent with other Data in GISAID and in accordance with GISAID's Database Access Agreement; and
-4.	You will provide proper attributions, acknowledgements, and efforts to collaborate with data submitters consistent with the GISAID's and Database Access Agreement when using Data in GISAID in publications, preprints, manuscripts, or any other analyses or media material.
+Reminder of GISAID's Database Access Agreement
 
-You can see the full terms of use at https://www.gisaid.org/registration/terms-of-use/. By using the outbreak.info R package you reaffirm your understanding of these terms and the DAA.
+Your ability to access and use Data in GISAID, including your access and
+use of same via R Package, is subject to the terms and conditions of
+GISAID's Database Access Agreement (“DAA”) (which you agreed to
+when you requested access credentials to GISAID), as well as the
+following terms:
 
-When using this data, please state, \"This data was obtained from GISAID via the outbreak.info API\". WE DO NOT SUPPORT THIRD PARTY APPLICATIONS. THIS PACKAGE IS MEANT FOR RESEARCH AND VISUALIZATION PURPOSES ONLY. If you want to build third party applications, please contact GISAID via https://www.gisaid.org/help/contact/.
+1. You will treat all data contained in the R Package consistent with
+other Data in GISAID and in accordance with GISAID's Database Access
+Agreement;
+
+2. You will not distribute, or re-distribute Data made available through
+GISAID to any third party other than Authorized Users as contemplated by
+the DAA;
+
+3. USE OF R PACKAGE: Any visualizations, charts, graphs,
+graphics, pictographs, plots, or other displays you create via the R
+Package may be exclusively used for academic and research purposes.
+No other types of uses are allowed;
+
+4. Any use of visualizations, charts, graphs, graphics, pictographs,
+plots, or other displays created via the R Package in an academic or
+research publication, including in a paper, manuscript, preprint, website,
+web service, or any other media material must be in conformity with the
+GISAID Publishing Guidelines, available at https://www.gisaid.org/publish,
+and the DAA, available at https://www.gisaid.org/daa; and
+
+5. By using the R Package you reaffirm your understanding of these
+terms and the DAA.
+
+When using this data, please state, \"This data was obtained from GISAID via the outbreak.info API\". 
+WE DO NOT SUPPORT THIRD PARTY APPLICATIONS. THIS PACKAGE IS MEANT FOR RESEARCH AND VISUALIZATION PURPOSES ONLY. 
+If you want to build third party applications, please contact GISAID via https://www.gisaid.org/help/contact/.
 "
-    cat(termsText)
+    cat(red$bold(termsText))
 }
